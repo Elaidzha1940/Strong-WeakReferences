94f96e9567c7f0e6883ca97a98edb38b18519161
Elaidzha1940 <eli080293@gmail.com>
2024-01-07 23:56:29 +0300

    StrongReferences

---
+++ StrongReferences/StrongReferences.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: StrongReferences
+//  File: StrongReferences.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 07.01.2024
+//
+//  */
+
+import SwiftUI
+
+struct StrongReferences: View {
+    var body: some View {
+        
+        VStack {
+         
+        }
+    }
+}
+
+#Preview {
+    StrongReferences()
+}
---
+++ StrongReferences/StrongReferencesApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: StrongReferences
+//  File: StrongReferencesApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 07.01.2024
+//
+//  */
+
+import SwiftUI
+
+@main
+struct StrongReferencesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            StrongReferences()
+        }
+    }
+}
